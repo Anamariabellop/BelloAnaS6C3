8c128d4559f2da7eb6711411ccba0ccb940e16a4
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-10 23:20:34 -0500

    make file caso 2 agregado

--- cuerdayTambor.mk
+++ cuerdayTambor.mk
@@ -7,12 +7,12 @@ cuerdacaso1.png: %.txt Plots_cuerdayTambor.py BelloAna_cuerdayTambor.cpp
 a.out: BelloAna_cuerdayTambor.cpp
 	g++ BelloAna_cuerdayTambor.cpp
 
-#cuerdacaso2.png: %.txt Plots_cuerda.py BelloAna_cuerda.cpp
-	#python Plots_cuerda.py
-#%.txt: a.out
-	#./a.out
-#a.out: BelloAna_cuerda.cpp
-	#g++ BelloAnaS5C2.cpp
+cuerdacaso2.png: %.txt Plots_cuerdayTambor.py BelloAna_cuerdayTambor.cpp
+	python Plots_cuerdayTambor.py
+%.txt: a.out
+	./a.out
+a.out: BelloAna_cuerdayTambor.cpp
+	g++ BelloAna_cuerdayTambor.cpp
 
 #cuerdacaso3.png: %.txt Plots_cuerda.py BelloAna_cuerda.cpp
 	#python Plots_cuerda.py
