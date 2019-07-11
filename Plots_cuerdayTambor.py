8c22370e8c377fa0d38216f97d3d574b33fced8b
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-10 23:20:01 -0500

    plot caso 2

--- Plots_cuerdayTambor.py
+++ Plots_cuerdayTambor.py
@@ -38,3 +38,42 @@ plt.title("Condicion Futura")
 
 plt.subplots_adjust(hspace=.7)
 plt.savefig("cuerdacaso1.png")
+
+caso2=np.genfromtxt("cuerdacaso2.txt")
+caso2b=np.genfromtxt("cuerda2caso2.txt")
+
+tc2=caso2[:,0]
+xc2=caso2[:,2]
+xpc2=caso2[:,3]
+
+t2c2=caso2b[:,0]
+xp2c2=caso2b[:,3]
+
+plt.figure()
+plt.subplot(3,1,1)
+plt.plot(tc2,xc2, color='g')
+plt.xlabel("L")
+plt.ylabel(" ")
+plt.grid()
+plt.ylim(-0.001,0.011)
+plt.title("Condiciones iniciales")
+
+plt.subplot(3,1,2)
+plt.plot(tc2,xpc2, color='g')
+plt.xlabel("L")
+plt.ylabel(" ")
+plt.grid()
+plt.ylim(-0.001,0.011)
+plt.title("Condiciones presente")
+
+plt.subplot(3,1,3)
+plt.plot(t2c2,xp2c2, color='g')
+plt.xlabel("L")
+plt.ylabel(" ")
+plt.grid()
+plt.ylim(-0.001,0.011)
+plt.title("Condicion Futura")
+
+plt.subplots_adjust(hspace=.7)
+plt.savefig("cuerdacaso2.png")
+
